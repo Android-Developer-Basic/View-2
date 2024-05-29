0d14fe18665f39f2b3bcf76ac6e120a320db0680
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-05-29 14:17:21 +0200

    Getting style

--- app/src/main/java/otus/gbp/view/components/LikeDislike.kt
+++ app/src/main/java/otus/gbp/view/components/LikeDislike.kt
@@ -6,6 +6,8 @@ import android.view.Gravity.CENTER
 import android.view.Gravity.CENTER_HORIZONTAL
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.withStyledAttributes
 import androidx.core.util.TypedValueCompat.dpToPx
 import androidx.core.util.TypedValueCompat.pxToDp
 import otus.gbp.view.R
@@ -15,7 +17,7 @@ class LikeDislike @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+) : LinearLayoutCompat(context, attrs, defStyleAttr) {
     private val binding: LikeDislikeBinding
     private var likes = 0
 
@@ -26,16 +28,33 @@ class LikeDislike @JvmOverloads constructor(
     }
 
     private fun initPanel(attrs: AttributeSet?, defStyleAttr: Int) {
-        layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        orientation = HORIZONTAL
-        gravity = CENTER
-        setPadding(
-            0,
-            dpToPx(8),
-            0,
-            dpToPx(8)
-        )
-        setBackgroundResource(R.drawable.like_bg)
+        // for android limitation, before finding attrs, you need to sort all attrs you want to find in ascending order, otherwise some attrs cannot be found
+        // https://developer.android.com/reference/android/content/res/Resources.Theme.html#obtainStyledAttributes(android.util.AttributeSet, int[], int, int)
+        val toRetrieve = intArrayOf(
+            android.R.attr.layout_width,
+            android.R.attr.layout_height,
+            android.R.attr.orientation,
+            android.R.attr.gravity,
+            android.R.attr.paddingTop,
+            android.R.attr.paddingBottom,
+            android.R.attr.background
+        ).apply { sort() }
+
+        context.withStyledAttributes(attrs, toRetrieve, defStyleAttr, R.style.like_dislike_panel) {
+            layoutParams = LayoutParams(
+                getInt(toRetrieve.indexOf(android.R.attr.layout_width), LayoutParams.WRAP_CONTENT),
+                getInt(toRetrieve.indexOf(android.R.attr.layout_height), LayoutParams.WRAP_CONTENT),
+            )
+            orientation = getInt(toRetrieve.indexOf(android.R.attr.orientation), HORIZONTAL)
+            gravity = getInt(toRetrieve.indexOf(android.R.attr.gravity), CENTER)
+            setPadding(
+                0,
+                getDimensionPixelSize(toRetrieve.indexOf(android.R.attr.paddingTop), 0),
+                0,
+                getDimensionPixelSize(toRetrieve.indexOf(android.R.attr.paddingBottom), 0),
+            )
+            background = getDrawable(toRetrieve.indexOf(android.R.attr.background))
+        }
     }
 
     private fun initLikes() = with(binding) {
