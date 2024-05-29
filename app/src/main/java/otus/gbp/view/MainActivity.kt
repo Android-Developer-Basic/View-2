35ef058f92255f2561ab35bf047c6888a9e6bfe6
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-05-29 12:10:07 +0200

    Like-dislike component base

--- app/src/main/java/otus/gbp/view/MainActivity.kt
+++ app/src/main/java/otus/gbp/view/MainActivity.kt
@@ -9,22 +9,10 @@ import otus.gbp.view.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var likes = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.main)
-        initLikes()
-    }
-
-    private fun initLikes() = with(binding) {
-        numLikes.text = likes.toString()
-        likeButton.setOnClickListener {
-            numLikes.text = (++likes).toString()
-        }
-        dislikeButton.setOnClickListener {
-            numLikes.text = (--likes).coerceAtLeast(0).toString()
-        }
     }
 }
--- app/src/main/java/otus/gbp/view/components/LikeDislike.kt
+++ app/src/main/java/otus/gbp/view/components/LikeDislike.kt
@@ -2,12 +2,31 @@ package otus.gbp.view.components
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.LinearLayout
+import otus.gbp.view.R
+import otus.gbp.view.databinding.LikeDislikeBinding
 
 class LikeDislike @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
-    // TODO: Реализация
+    private val binding: LikeDislikeBinding
+    private var likes = 0
+
+    init {
+        binding = LikeDislikeBinding.inflate(LayoutInflater.from(context), this)
+        initLikes()
+    }
+
+    private fun initLikes() = with(binding) {
+        numLikes.text = likes.toString()
+        likeButton.setOnClickListener {
+            numLikes.text = (++likes).toString()
+        }
+        dislikeButton.setOnClickListener {
+            numLikes.text = (--likes).coerceAtLeast(0).toString()
+        }
+    }
 }
